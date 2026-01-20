91b7280f76074eceb85901763177b8dfe75bed46
Vipin Singh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-01-20 06:50:30 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (77.81%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,23 +1,55 @@
+// class Solution {
+// public:
+//     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+//         vector<int>ans;
+//         for(int i=0;i<nums1.size();i++){
+//             for(int j=0;j<nums2.size();j++){
+//                 if(nums1[i]==nums2[j]){
+//                     bool exist = false;
+//                     for(int k=0;k<ans.size();k++){
+//                         if(ans[k]==nums1[i]){
+//                             exist = true;
+//                             break;
+//                         }
+//                     }
+//                     if(!exist){
+//                     ans.push_back(nums1[i]);
+//                     }
+//                     break;
+//                 }
+                
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+//optimal
+
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans;
-        for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    bool exist = false;
-                    for(int k=0;k<ans.size();k++){
-                        if(ans[k]==nums1[i]){
-                            exist = true;
-                            break;
-                        }
-                    }
-                    if(!exist){
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        vector<int> ans;
+        int i = 0, j = 0;
+
+        while(i < nums1.size() && j < nums2.size()) {
+
+            if(nums1[i] == nums2[j]) {
+                // avoid duplicates
+                if(ans.empty() || ans.back() != nums1[i]) {
                     ans.push_back(nums1[i]);
-                    }
-                    break;
                 }
-                
+                i++;
+                j++;
+            }
+            else if(nums1[i] < nums2[j]) {
+                i++;
+            }
+            else {
+                j++;
             }
         }
         return ans;
