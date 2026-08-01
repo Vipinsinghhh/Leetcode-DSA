f750b1580a9ef56ea07fbfcd7844c052a6924aea
Vipin Singh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-08-01 23:50:37 +0530

    Time: 0 ms (100.00%) | Memory: 24.4 MB (6.44%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,30 +2,33 @@ class Solution {
 public:
     vector<int>moveZeroes(vector<int>& nums) {
 
-        // int j = 0;
+//------------------------optimised-------------------------
+        int j = 0;
 
-        // for (int i = 0; i < nums.size(); i++) {
-        //     if (nums[i] != 0) {
-        //         swap(nums[i], nums[j]);
-        //         j++;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+        return nums;
+
+//-----------------brute force -------------------------
+        // vector<int>temp(nums.size(), 0);
+
+        // int tempIndex = 0;
+
+        // for(int i =0; i<nums.size(); i++){
+        //     if(nums[i] != 0){
+        //         temp[tempIndex] = nums[i];
+        //         tempIndex++;
         //     }
         // }
 
-        vector<int>temp(nums.size(), 0);
+        // for(int i =0; i<temp.size();i++){
+        //     nums[i] = temp[i];
+        // }
 
-        int tempIndex = 0;
-
-        for(int i =0; i<nums.size(); i++){
-            if(nums[i] != 0){
-                temp[tempIndex] = nums[i];
-                tempIndex++;
-            }
-        }
-
-        for(int i =0; i<temp.size();i++){
-            nums[i] = temp[i];
-        }
-
-        return nums;
+        // return nums;
     }
 };
